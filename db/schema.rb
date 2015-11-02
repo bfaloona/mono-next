5cc2036498e54e95599156f8dc1b0ac8c7ada371
Brandon Faloona <brandon@faloona.net>
2015-11-01 17:01:56 -0800

    Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -61,12 +61,4 @@ ActiveRecord::Schema.define(version: 20100519065234) do
     t.string   "classification", limit: 255
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
