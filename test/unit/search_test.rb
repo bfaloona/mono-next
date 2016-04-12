38afe6a5c62ece485bf6d36bdf6ff7807210c47e
Brandon Faloona <brandon@faloona.net>
2016-04-12 00:03:34 -0700

    New query tests. Test description updates

--- test/unit/search_test.rb
+++ test/unit/search_test.rb
@@ -2,26 +2,46 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 describe 'Monologues Search Gender Men' do
 
-  it ' /search/pinapples/m returns no pinapples!' do
+  it ' Valid query for missing term returns no matches' do
     get '/search/pinapples/m'
     monologues_displayed(last_response).must_equal 0
   end
 
-  it ' /search/twenty/m finds multiple matches' do
+  it ' General query returns multiple matches' do
     get '/search/twenty/m'
     monologues_displayed(last_response).must_equal 3
   end
 
-  it ' /search/amoung%20twenty/m returns one match' do
+  it ' Exact query returns one match' do
     get '/search/among%20twenty/m'
     monologues_displayed(last_response).must_equal 1
-
   end
 
-  it ' /search/S/hame/m returns case insensitive results' do
+  it ' Uppercase query returns case insensitive matches' do
     get '/search/Shame/m'
     monologues_displayed(last_response).must_equal 5
   end
+
+  it ' Punctuation in query returns correct match' do
+    get '/search/of%20battles!%20steel%20my%20soldiers\'%20hearts/m'
+    last_response.body.must_include 'IV i 178'
+  end
+
+  it ' Trailing space in query returns correct match' do
+    get '/search/my%20complete%20master%20/m'
+    last_response.body.must_include 'my complete master'
+  end
+
+  #########
+  # Pending
+  #
+
+  it ' Sans Punctuation in query returns correct match' do
+    skip 'Not yet supported'
+    get '/search/of%20battles%20steel%20my%20soldiers%20hearts/m'
+    last_response.body.must_include 'IV i 178'
+  end
+
 
 end
 
