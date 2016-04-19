fb55e0a57817508587b2237d5a5ae2a9f5087667
Brandon Faloona <brandon@faloona.net>
2016-04-19 07:37:16 -0700

    Tests for g querystring params

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -7,6 +7,18 @@ describe 'Monologues routing' do
     last_response.body.must_include 'As You Like It'
   end
 
+  it ' /plays/23?g=2 returns women filtered play' do
+    get '/plays/23?g=2'
+    last_response.body.must_include 'My Lord of Suffolk'
+    last_response.body.must_include '1 of 1 monologues'
+  end
+
+  it ' /plays/23?g=3 returns men filtered play' do
+    get '/plays/23?g=3'
+    last_response.body.must_include 'Is Cade the son of Henry the Fifth'
+    last_response.body.must_include '2 of 2 monologues'
+  end
+
   it ' /monologues/813 returns: I cannot heave my heart' do
     get '/monologues/813'
     last_response.body.must_include 'Return those duties back as are right fit'
